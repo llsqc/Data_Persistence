4af57071d70f4b8d890fb1d96d255506c170db09
isolation <23796978@qq.com>
2025-04-12 22:29:23 +0800

    feat: 完成XMLDataMgr

--- Assets/Scripts/XMLDataMgr/XMLDataMgr.cs
+++ Assets/Scripts/XMLDataMgr/XMLDataMgr.cs
@@ -1,16 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
 using UnityEngine;
 
-public class XMLDataMgr : MonoBehaviour
+public class XMLDataMgr
 {
-    void Start()
+    private static XMLDataMgr _instance = new XMLDataMgr();
+
+    public static XMLDataMgr Instance => _instance;
+
+    private XMLDataMgr()
     {
-        
     }
 
-    void Update()
+    public void SaveData(object data, string fileName)
     {
+        string path = $"{Application.persistentDataPath}/{fileName}.xml";
+
+        using (StreamWriter writer = new StreamWriter(path))
+        {
+            XmlSerializer serializer = new XmlSerializer(data.GetType());
+            serializer.Serialize(writer, data);
+        }
+    }
+
+    public object LoadData(Type type, string fileName)
+    {
+        string path = $"{Application.persistentDataPath}/{fileName}.xml";
+
+        if (!File.Exists(path)) path = $"{Application.streamingAssetsPath}/{fileName}.xml";
+        if (!File.Exists(path)) return Activator.CreateInstance(type);
         
+        using (StreamReader reader = new StreamReader(path))
+        {
+            XmlSerializer serializer = new XmlSerializer(type);
+            return serializer.Deserialize(reader);
+        }
     }
 }
